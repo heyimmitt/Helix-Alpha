e66bdfecf787dd2e127b7601ed0ddd6d69fa8a66
SEshaan <greatesh2006@gmail.com>
2025-08-10 01:01:17 +0530

    ui update and script update

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -10,25 +12,39 @@ public class Movement : MonoBehaviour
     public float movementSpeed = 10f;
 
     private bool gravityInverted = false;
-
     public float gravityMultiplier = 2f;
+
+    public GameObject WinScreen;
+    public GameObject DeathScreen;
+
+    public TMP_Text scoreMessage;
+
+
+    public int scoreMultiplier = 1;
 
     void Start()
     {
         player = this.gameObject;
         rb = this.gameObject.GetComponent<Rigidbody>();
 
+
+        WinScreen.SetActive(false);
+        DeathScreen.SetActive(false);
+
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)){
+        if (Input.GetKey(KeyCode.A))
+        {
             tunnel.transform.Rotate(0f, 0f, rotspeed * Time.deltaTime);
-        } else if(Input.GetKey(KeyCode.D)){
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
             tunnel.transform.Rotate(0f, 0f, -rotspeed * Time.deltaTime);
         }
-        
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x,rb.linearVelocity.y,movementSpeed);
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, movementSpeed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -36,6 +52,8 @@ public class Movement : MonoBehaviour
             Physics.gravity = gravityMultiplier * (gravityInverted ? Vector3.up * 9.81f : Vector3.down * 9.81f);
 
         }
+
+        scoreMessage.text = "Score: " + ComputeScore();
     }
 
     // This will be called when the player collides with another collider
@@ -43,8 +61,32 @@ public class Movement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("KillBlock"))
         {
+            DeathScreen.SetActive(true);
             Destroy(player);
+            SceneManager.LoadScene(0);
+
         }
+        else if (collision.gameObject.CompareTag("EndBlock"))
+        {
+            WinScreen.SetActive(true);
+            Destroy(player);
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private int ComputeScore()
+    {
+        return Mathf.RoundToInt(scoreMultiplier * player.transform.position.z);
+    }
+    
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
--- Assets/Scripts/follow.cs
+++ Assets/Scripts/follow.cs
@@ -14,6 +14,10 @@ public class follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (target == null)
+        {
+            return;
+        }
         obj.transform.position = new Vector3(obj.transform.position.x + offset.x, obj.transform.position.y, target.transform.position.z + offset.z);
     }
 }
