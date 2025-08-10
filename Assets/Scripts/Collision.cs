2100c9b30d618d9a03c2612e5619fd12bff3ccce
NaeshaMehta <naeshamehta@gmail.com>
2025-08-10 13:29:08 +0530

    Refactored Code

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject WinScreen;
+    public GameObject DeathScreen;
+    public TMP_Text scoreMessage;
+
+    public int scoreMultiplier = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        WinScreen.SetActive(false);
+        DeathScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreMessage.text = "Score: " + ComputeScore();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("KillBlock"))
+        {
+            DeathScreen.SetActive(true);
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(0);
+
+        }
+        else if (collision.gameObject.CompareTag("EndBlock"))
+        {
+            WinScreen.SetActive(true);
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private int ComputeScore()
+    {
+        return Mathf.RoundToInt(scoreMultiplier * transform.position.z);
+    }
+
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
